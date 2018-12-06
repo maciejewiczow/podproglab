d140d8d9d1759da4efd185e16d690bd7988d948d
Mafciejewicz <melonik0.9846@gmail.com>
2018-12-06 04:36:24 +0100

    Implemented methods to calculate approximations of e

---
+++ lab7291118/szeregi/euler.c
@@ -0,0 +1,68 @@
+#include <float.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#if 1
+#    define SCALAR double
+#    define FORMAT_IN "%lf"
+#    define FORMAT_OUT "%5.20lf"
+#    define EPSILON DBL_EPSILON
+#else
+#    define SCALAR float
+#    define FORMAT_IN "%f"
+#    define FORMAT_OUT "%5.20f"
+#    define EPSILON FLT_EPSILON
+#endif
+
+#define ITERATION_LIMIT 1.e+8
+
+SCALAR eulerFromSeries(int n);
+SCALAR eulerFromErrorTolerance(SCALAR tolerance);
+bool veryClose(SCALAR, SCALAR, SCALAR);
+
+int main(void)
+{
+    SCALAR precision;
+
+    printf("Hello, please tell me, how precisely should I calculate e for you:\n");
+    scanf(FORMAT_IN, &precision);
+
+    SCALAR result = eulerFromErrorTolerance(precision);
+    printf("\ne ~= " FORMAT_OUT "\nError: " FORMAT_OUT, result, fabs(result - M_E));
+}
+
+SCALAR eulerFromSeries(int n)
+{
+    if (n == 0) return 1;
+
+    SCALAR partial = ((SCALAR) 1.0 + ((SCALAR) 1.0) / n);
+    return (sizeof(SCALAR) == 8) ? pow(partial, n) : powf(partial, n);
+}
+
+bool veryClose(SCALAR a, SCALAR b, SCALAR tolerance)
+{
+    return a == b || (sizeof(SCALAR) == 4) ? fabsf(a - b) < tolerance : fabs(a - b) < tolerance;
+}
+
+SCALAR eulerFromErrorTolerance(SCALAR tolerance)
+{
+    if (tolerance <= EPSILON) {
+        printf("\nI am nothing but a mere rock, that people tricked into doing math (because they "
+               "were too lazy to do it by themeselves). I can't perform computations THAT precise, "
+               "sorry.\n");
+        return -1;
+    }
+
+    SCALAR result;
+    int n = 1;
+    do {
+        result = eulerFromSeries(n);
+        // printf("n: %d " FORMAT_OUT "\n", n, result);
+        n++;
+    } while (n < ITERATION_LIMIT && !veryClose(result, M_E, tolerance));
+
+    printf("\nItertions: %d\n", n);
+
+    return result;
+}
