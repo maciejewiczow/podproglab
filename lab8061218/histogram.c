0431a26c15e0346c6a31102db35cb17dd9381cf8
Maciek Adamus <melonik0.9846@gmail.com>
2018-12-10 20:45:07 +0100

    Made the string and range bounds variable, values taken from stdin * Refractored data collection to separate function * Added allocation check when allocating dictionary entries * changed dict length to unsigned int

--- lab8061218/histogram.c
+++ lab8061218/histogram.c
@@ -1,14 +1,11 @@
+#include <assert.h>
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define ARRAY_LENGTH 200
-
 #define DEBUG_PRINTS false
-
-#define RANGE_START '0'
-#define RANGE_END 'z'
 
 typedef struct Entry {
     char key;
@@ -16,48 +13,72 @@ typedef struct Entry {
 } DictEntry;
 
 typedef struct Dict {
-    int length;
-    int end;
+    unsigned int length;
+    unsigned int end;
     DictEntry* entries;
 } Dictionary;
-void dict_new(Dictionary*, int);
+void dict_new(Dictionary*, unsigned int);
 void dict_delete(Dictionary*);
 DictEntry* dict_getEntryByKey(const Dictionary*, const char);
-int dict_addEntry(Dictionary*, char, int);
+int dict_addEntry(Dictionary*, const char, const int);
 void dict_print(const Dictionary*);
 void dict_sort(Dictionary*);
 
 void fillArrayWithRandomValues(char*, const int, const char, const char);
-
+void createStringHistorgram(Dictionary*, const char*);
 void printHistogramFromDict(const Dictionary*);
+int max(int, int);
+int min(int, int);
 
 int main(void)
 {
-    char array[ARRAY_LENGTH];
     srand(time(0));
+    unsigned int arrayLength = 50;
+    char start = 'a';
+    char end = 'z';
+
+    printf("Please enter length of string to analise: ");
+    scanf("%u", &arrayLength);
+
+    printf("Please enter bounding characters of (inclusive) range, from which random string will "
+           "be generated:\nstart: ");
+    scanf(" %c", &start);
+    printf("end: ");
+    scanf(" %c", &end);
+
+#if DEBUG_PRINTS
+    printf("%c(%d) %c(%d)\n", start, (int) start, end, (int) end);
+#endif
+
+    int temp = (int) start;
+    start = (char) min(start, end);
+    end = (char) max(temp, end);
+
+    assert(start < end);
+
+#if DEBUG_PRINTS
+    printf("%c - %c\n", start, end);
+#endif
+
+    char* array = (char*) calloc(arrayLength, sizeof(char));
+    if (array == NULL) {
+        printf("Memory Error: couldn't allocate enough memory for string. Aborting");
+        return -1;
+    }
+
+#if DEBUG_PRINTS
+    printf("%s\n", array);
+#endif
+
+    fillArrayWithRandomValues(array, arrayLength, start, end);
+    array[arrayLength - 1] = '\0';
 
     Dictionary dict;
-    dict_new(&dict, (RANGE_END - RANGE_START) + 1);
-
-    fillArrayWithRandomValues(array, ARRAY_LENGTH, RANGE_START, RANGE_END);
+    dict_new(&dict, (end - start) + 1);
 
     printf("%s\n\n", array);
 
-    for (int i = 0; i < ARRAY_LENGTH; i++) {
-        DictEntry* e = dict_getEntryByKey(&dict, array[i]);
-
-        if (e == NULL) {
-            int error = dict_addEntry(&dict, array[i], 1);
-            if (error) {
-                printf(
-                    "Error (%d), dictionary is too small for all this values. Aborting\n", error);
-                return -1;
-            }
-        }
-        else {
-            e->value++;
-        }
-    }
+    createStringHistorgram(&dict, array);
 
     dict_sort(&dict);
 
@@ -68,19 +89,53 @@ int main(void)
     printHistogramFromDict(&dict);
 
     dict_delete(&dict);
+    free(array);
 
     return 0;
 }
 
+int max(int a, int b)
+{
+    return (a > b) ? a : b;
+}
+int min(int a, int b)
+{
+    return (a < b) ? a : b;
+}
+
+void createStringHistorgram(Dictionary* dict, const char* string)
+{
+    int i = 0;
+    char c;
+
+    while ((c = string[i]) != '\0') {
+        DictEntry* e = dict_getEntryByKey(dict, c);
+
+        if (e == NULL) {
+            int error = dict_addEntry(dict, c, 1);
+            if (error) {
+                printf(
+                    "Error (%d): dictionary is too small for all this values. Aborting\n", error);
+                exit(-1);
+            }
+        }
+        else {
+            e->value++;
+        }
+        i++;
+    }
+}
+
 void fillArrayWithRandomValues(char* arr, const int length, const char min, const char max)
 {
-    for (int i = 0; i < length; i++)
+    // leaving null terminator intact or a free slot if there wasn't one to begin with
+    for (int i = 0; i < (length - 1); i++)
         arr[i] = (char) (random() % (max - min + 1)) + min;
 }
 
 void printHistogramFromDict(const Dictionary* dict)
 {
-    for (int i = 0; i < dict->end; i++) {
+    for (unsigned int i = 0; i < dict->end; i++) {
         DictEntry record = dict->entries[i];
         printf("%c  ", record.key);
         for (int j = 0; j < record.value; j++)
@@ -89,13 +144,17 @@ void printHistogramFromDict(const Dictionary* dict)
     }
 }
 
-void dict_new(Dictionary* this, int numberOfRecords)
+void dict_new(Dictionary* this, unsigned int numberOfRecords)
 {
     this->length = numberOfRecords;
     this->end = 0;
 
-    DictEntry* p;
-    p = (DictEntry*) calloc(numberOfRecords, sizeof(DictEntry));
+    DictEntry* p = (DictEntry*) calloc(numberOfRecords, sizeof(DictEntry));
+
+    if (p == NULL) {
+        printf("Memory Error: Could not allocate memory for the dictionary");
+        exit(-1);
+    }
 
     this->entries = p;
 }
@@ -109,14 +168,14 @@ void dict_print(const Dictionary* this)
 {
     printf("Length: %d\nEnd: %d\n", this->length, this->end);
 
-    for (int i = 0; i < this->end; i++)
+    for (unsigned int i = 0; i < this->end; i++)
         printf("%d   (int %d) %c: %d\n", i, (int) this->entries[i].key, this->entries[i].key,
             this->entries[i].value);
 }
 
 DictEntry* dict_getEntryByKey(const Dictionary* this, const char key)
 {
-    for (int i = 0; i < this->end; i++) {
+    for (unsigned int i = 0; i < this->end; i++) {
         if (this->entries[i].key == key) return &this->entries[i];
     }
     return NULL;
@@ -128,7 +187,7 @@ void dict_sort(Dictionary* this)
 
     do {
         swapped = false;
-        for (int i = 0; i < this->end - 1; i++) {
+        for (unsigned int i = 0; i < this->end - 1; i++) {
             if (this->entries[i].key > this->entries[i + 1].key) {
                 DictEntry temp = this->entries[i];
                 this->entries[i] = this->entries[i + 1];
