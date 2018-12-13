5c0001afdeb0b5d9a82684e1cef3e9796d10d193
Maciek Adamus <melonik0.9846@gmail.com>
2018-12-13 14:25:39 +0100

    Extracted square root code to function and documented it with comment

--- lab9131218/pierwiastek/pierwiastek.c
+++ lab9131218/pierwiastek/pierwiastek.c
@@ -3,9 +3,17 @@
 
 #define ACCURACY (1.e-5) // do przeprowadzania obliczeń i sprawdzenia dokładności wyniku
 
-void main(void)
-{
+#define DEBUG_PRINTS 1
 
+/**
+ * Funkcja obliczająca pierwiastek danej liczby zmiennoprzecinkowej za pomocą wzoru Herona
+ * @param double x - liczba pierwiastkowana
+ * @returns double - wynik pierwiastkowania lub -1 w przypadku błędnych danych (x < 0)
+ */
+double squareRoot(double x);
+
+int main(void)
+{
     double liczba;
 
     for (;;) {
@@ -13,48 +21,41 @@ void main(void)
         printf("\nPodaj wartość liczby dodatniej (-1 kończy program):\n");
         scanf("%lf", &liczba);
 
-        if (liczba < 0.0) break;
+        if (liczba < 0) return 0;
 
-        double pierwiastek = 1.0;
-        double temp;
-        // obliczenia pierwiastka za pomocą wzoru Herona
-        do {
-            temp = pierwiastek;
-            pierwiastek = 0.5 * (temp + liczba / temp);
-
-            // sprawdzenie warunku wyjścia z pętli - błąd względny obliczania pierwiastka
-        } while (fabs(pierwiastek * pierwiastek - liczba) / liczba > ACCURACY);
+        double result = squareRoot(liczba);
+        double refference = sqrt(liczba);
 
         printf("\nliczba %20.15lf, założona dokładność obliczania pierwiastka: %20.15lf\n", liczba,
             ACCURACY);
-        printf("\tpierwiastek liczby: obliczony %20.15lf, dokładny %20.15lf\n", pierwiastek,
-            sqrt(liczba));
-        printf("\tbłąd bezwzględny: %20.15lf, błąd względny: %20.15lf\n",
-            pierwiastek - sqrt(liczba), (pierwiastek - sqrt(liczba)) / sqrt(liczba));
+        printf("\tpierwiastek liczby: obliczony %20.15lf, dokładny %20.15lf\n", result, refference);
+        printf("\tbłąd bezwzględny: %20.15lf, błąd względny: %20.15lf\n", result - refference,
+            (result - refference) / refference);
     }
+    return 0;
 }
 
-// definicja funkcji + komentarze (z komentarzy można tworzyć dokumentację):
+double squareRoot(double x)
+{
+    if (x < 0.0) return -1;
 
-// funkcja oblicz_pierwiastek oblicza pierwiastek za pomocą wzoru Herona
-//               dla dowolnej liczby podwójnej precyzji większej od zera
-// wariant 1. funkcja zakłada, że argument jest nie mniejszy niż zero
-// wariant 2. dla liczb mniejszych od zera funkcja zwraca kod błędu: -1
-//   implementacja w kodzie, np.
-//  if(arg<0){
-//    // obsługa błędu wejścia, np.
-//    return(-1.0); // wyjaśnienie w dokumentacji
-//  }
+    double result = 1.0;
+    double prev;
 
-// wydruki kontrolne do sprawdzenia działania funkcji:
+#if DEBUG_PRINTS
+    printf("Aktualna obliczona wartość, \tbłąd względny kolejnych iteracji, \tbłąd wzgledny "
+           "kwadratu\n");
+#endif
+    // obliczenia pierwiastka za pomocą wzoru Herona
+    do {
+        prev = result;
+        result = 0.5 * (prev + x / prev);
+#if DEBUG_PRINTS
+        printf("wartość %20.15lf, \tbłąd  %20.15lf,     błąd kwadratu %20.15lf\n", result,
+            fabs(result - prev) / result, fabs(result * result - x) / x);
+#endif
+        // sprawdzenie warunku wyjścia z pętli - błąd względny obliczania pierwiastka
+    } while (fabs(result * result - x) / x > ACCURACY);
 
-//  printf("aktualna obliczona wartość, \tbłąd względny kolejnych iteracji, \tbłąd wzgledny
-//  kwadratu\n");
-//
-// do{ ....
-//
-//    printf("wartość %20.15lf, \tbłąd  %20.15lf,     błąd kwadratu %20.15lf\n",
-//	   pierwiastek, fabs(pierwiastek - temp)/pierwiastek,
-//	   fabs(pierwiastek*pierwiastek - arg)/arg);
-//
-//    ....}
+    return result;
+}
