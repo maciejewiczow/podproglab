8d5cb1434b038be9fa3ed10f234e4bd345308154
Maciek Adamus <melonik0.9846@gmail.com>
2019-01-06 04:44:58 +0100

    Modified build pipeline, so that it could handle multiple files per exec

--- compile.sh
+++ compile.sh
@@ -2,8 +2,16 @@
 
 pathNoExt=$(echo "$1" | cut -f 1 -d '.')
 
+if [ -f "$2/makefile" ]; then
+
+cd $2
+make buildAndRun
+
+else
+
 gcc -Wall -Wextra "$pathNoExt.c" -o "$pathNoExt.out" -lm
 
 if [ $? -eq 0 ]; then
  "$pathNoExt.out"
+fi
 fi
