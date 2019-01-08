1f3ed086448e8810018a2c281341464fa4989270
Maciek Adamus <melonik0.9846@gmail.com>
2019-01-08 15:10:35 +0100

    Debugging setup * Made non-make script also use external terminal

--- compile.sh
+++ compile.sh
@@ -5,13 +5,14 @@ pathNoExt=$(echo "$1" | cut -f 1 -d '.')
 if [ -f "$2/makefile" ]; then
 
 cd $2
-make buildAndRun
+make run
 
 else
 
 gcc -Wall -Wextra "$pathNoExt.c" -o "$pathNoExt.out" -lm
 
 if [ $? -eq 0 ]; then
- "$pathNoExt.out"
+    gnome-terminal --profile holdopen -- ./"$pathNoExt.out"
+	wmctrl -a 'Terminal'
 fi
 fi
