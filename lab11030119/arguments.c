101e367fa5606258503ee4c3ee88dd8f131ee5a2
Maciek Adamus <melonik0.9846@gmail.com>
2019-01-13 04:20:37 +0100

    Implemented  handlers for arguments and options

---
+++ lab11030119/arguments.c
@@ -0,0 +1,134 @@
+#include "getopt.h"
+#include "stdbool.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "unistd.h"
+
+#include "arguments.h"
+
+/**
+ * Helper function, used to load file contents into 3x3 array of bools
+ * value 1 is treated like true, every other thing is treated as false.
+ * For more information, refer to 'Input files' section in README.md
+ */
+static int loadNeighbourhoodFromFile(char*, bool[3][3]);
+
+void initalizeWithArguments(int argc, char* args[], Settings* setup)
+{
+    if (argc < 2) {
+        // no arguments supplied, print help and exit
+        printf(HELPTEXT, args[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    // initalize with defaults
+    setup->boardFilename = NULL;
+    setup->cellSurviveNumbers = "23";
+    setup->createNewCellNumbers = "3";
+    setup->delay = (struct timespec){
+        .tv_sec = 1,
+        .tv_nsec = 0,
+    };
+
+    for (int i = 0; i < 3; i += 2)
+        for (int j = 0; j < 3; j++)
+            setup->neighbourhood[i][j] = 1;
+
+    setup->neighbourhood[1][0] = 1;
+    setup->neighbourhood[1][1] = 0;
+    setup->neighbourhood[1][2] = 1;
+
+    const char* shortops = "d:c:s:n:h";
+    const struct option longopts[] = {
+        {"delay", required_argument, NULL, 'd'},
+        {"createnum", required_argument, NULL, 'c'},
+        {"survivenum", required_argument, NULL, 's'},
+        {"neigbourhood", required_argument, NULL, 'n'},
+        {"help", no_argument, NULL, 'h'},
+        {0, 0, 0, 0},
+    };
+
+    int c;
+
+    while ((c = getopt_long(argc, args, shortops, longopts, NULL)) != -1) {
+        switch (c) {
+            case 'd':;
+                long ms = atol(optarg);
+
+                setup->delay = (struct timespec){
+                    .tv_sec = (long) (ms / 1000),
+                    .tv_nsec = (ms % 1000) * 1000000L,
+                };
+                break;
+            case 'c':
+                setup->createNewCellNumbers = optarg;
+                break;
+            case 's':
+                setup->cellSurviveNumbers = optarg;
+                break;
+            case 'n':;
+                memset(&setup->neighbourhood, 0, sizeof(setup->neighbourhood));
+                int error = loadNeighbourhoodFromFile(optarg, setup->neighbourhood);
+
+                if (error) {
+                    printf("Could not load neigbourhood from file %s. Aborting", optarg);
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            case 'h':
+                printf(HELPTEXT, args[0]);
+                exit(EXIT_SUCCESS);
+                break;
+            default:
+                // in case of '?' getopt_long will print out appropiate error message for me, so
+                // just exit, other values not possible (afaik)
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    if (optind == argc) {
+        // after parsing options there was nothing left = only options were provided, w/out filename
+        printf("No file to read initial board state from given. Run with -h or --help for usage "
+               "instructions\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (optind < argc) {
+        // take first of remaining args as filename, ignore the rest
+        setup->boardFilename = args[optind];
+    }
+}
+
+static int loadNeighbourhoodFromFile(char* filename, bool neighbourhood[3][3])
+{
+    FILE* f = fopen(filename, "r");
+
+    if (f == NULL) return -1;
+
+    int c, row = 0, col = 0;
+    while ((c = getc(f)) != EOF) {
+        if (row == 1 && col == 1) {
+            col++;
+            continue;
+        }
+
+        if ((int) (col / 2) == 3) col = 0;
+
+        if (row == 3) break;
+
+        switch (c) {
+            case '\n':
+                row++;
+                col = 0;
+                break;
+            case '1':
+                neighbourhood[row][col] = true;
+                // fall-through
+            default:
+                col++;
+        }
+    }
+
+    return 0;
+}
---
+++ lab11030119/arguments.h
@@ -0,0 +1,53 @@
+#ifndef __GUARD_ARGS_H
+#define __GUARD_ARGS_H
+
+#include "stdbool.h"
+#include "time.h"
+#include "unistd.h"
+
+#define HELPTEXT                                                                                   \
+    "Usage: %s <file> [OPTIONS]\n"                                                                 \
+    "\t-d --delay\tset ms of delay between simulation time ticks. \n\t\t0 means that program "     \
+    "will wait for keypress between each tick (default 1000ms)\n"                                  \
+    "\t-c --createnum\t numbers of nearby alive cells, that cause new cell to be born (default "   \
+    "3)\n"                                                                                         \
+    "\t-s --survivenum \tnumbers of nearby alive cells, that provide survival of given cell "      \
+    "(default 23)\n"                                                                               \
+    "\n\t-n --neighbourhood\tname of file containing neigbourhood mask, that will be used in "     \
+    "simulation. Default is Moore's neighbourghood\n"                                              \
+    "\t-h --help \t displays this help text\n"
+
+/**
+ * Holds parameters (rules) used to perform the simulation.
+ *
+ * bool neigbourhood[3][3] - 3x3 matrix of booleans, used as a bit mask to determine rules of
+ *      neighbourhood, when counting number of alive neighbours of a given cell. Middle term's value
+ *      ([1][1]) is always ignored.
+ * struct timespec delay - struct passed as an argument to nanosleep().
+ *      Determines the duration of pause between time ticks. If NULL, then each frame is confirmed
+ *      manually with an ENTER key.
+ * char* boardFilename - name of a file, from which initial board config will be loaded
+ * char* cellSurviveNumbers - numbers of neighbouring cells, for whitch given cell can continue to
+ *      live. Each character of string is treated as a single digit. Non-digit chars are ignored.
+ * char* createNewCellNumbers - numbers of alive neighbours required for a new cell to be born in an
+ *      empty place. Format is the same as in previous field.
+ */
+typedef struct settings {
+    bool neighbourhood[3][3];
+    struct timespec delay;
+    char* boardFilename;
+    char* cellSurviveNumbers;
+    char* createNewCellNumbers;
+} Settings;
+
+/**
+ * Parses script arguments passed to main() in argc and argv and fills passed settings struct with
+ * appropiate values, eithier entered by user or defaults
+ *
+ * @param int argc - argument count passed to main()
+ * @param char*[] argv - argument vector passed to main()
+ * @param Settings* - pointer to settings struct
+ */
+void initalizeWithArguments(int, char* [], Settings*);
+
+#endif
